b93c0b62e4cc7489fa7e5a849c3ebd8467f24236
Rui Pinheiro <rui@ruipinheiro.eu>
2019-03-15 22:01:12 +0000

    Refactor syscall hook to separate file + clean up repository

--- 484/DEH/stage2/main.c
+++ 484/DEH/stage2/main.c
@@ -33,6 +33,7 @@
 #include "sm_ext.h"
 #include "laboratory.h"
 #include "ps3mapi_core.h"
+#include "syscall_hook.h"
 
 // Format of version:
 // byte 0, 7 MS bits -> reserved
@@ -1042,8 +1043,10 @@ int main(void)
 	permissions_patches();
 	
 #ifdef DEBUG
+	// Syscall Hook
+	syscall_hook_init();
+
 	// "Laboratory"
-	do_hook_all_syscalls();
 	//do_dump_threads_info_test();
 	//do_dump_processes_test();
 	//do_hook_load_module();
---
+++ 484/DEH/stage2/syscall_hook.h
@@ -0,0 +1,8 @@
+#ifndef __SYSCALL_HOOK_H__
+#define __SYSCALL_HOOK_H__
+
+void syscall_hook_init(void);
+
+#endif
+
+ 
